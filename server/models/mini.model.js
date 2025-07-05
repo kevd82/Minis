486dd9a4500e15a448ddbe94b114becb788c319a
Your Name <barbaricsplendor@gmail.com>
2025-07-04 17:18:23 -0700

    schema work

--- server/models/mini.model.js
+++ server/models/mini.model.js
@@ -11,6 +11,24 @@ const MiniSchema = new mongoose.Schema({
     company: {
         type:  String,
         required:  [true, "Company name required; enter --Unknown-- if needed."],
-        maxLength: [30, "Company must be 30 characters or less"]
-        }
+        maxLength: [30, "Company must be 30 characters or less!"]
+        },
+
+    code:  {
+        type: String,
+        required: [true, "Code required; enter --Unknown-- if needed."],
+        maxLength: [30, "Code must be 30 characters or less!"],
+    },
+
+    year:  {
+        type: String,
+        required: [true, "Year required; enter --Unknown-- if needed."],
+        maxLength: [4, "Year must be 4 digits or less!"],
+    },
+
+    sculptor:  {
+        type: String,
+        required: [true, "Sculptor required; enter --Unknown-- if needed."],
+        maxLength: [30, "Sculptor must be 30 characters or less!"],
+    },
 })
