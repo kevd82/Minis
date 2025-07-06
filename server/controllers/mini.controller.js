51eb3b56a38456ca3cc1994825a68a9799d287a9
Your Name <barbaricsplendor@gmail.com>
2025-07-05 22:01:00 -0700

    Mini.controller work

--- server/controllers/mini.controller.js
+++ server/controllers/mini.controller.js
@@ -1,2 +1,29 @@
 const Miniature = require("../models/mini.model");
 
+module.exports = {
+
+    findAllMinis: (req, res) =>{
+
+        Miniature.find()
+            .then((allMinis)=>{
+                console.log(allMinis);
+                res.json(allMinis)
+            })
+            .catch((err)=>{
+                console.log("findAllMinis has failed");
+                res.json({message: "Something went wrong in findAllMinis", error: err})
+            })
+    },
+
+    createNewMini: (req, res)=> {
+        Miniature.create()
+    }
+
+
+
+
+
+
+
+
+}
