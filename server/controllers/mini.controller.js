c7c9c0941dfe78248ce478f00a49789b30b95781
Your Name <barbaricsplendor@gmail.com>
2025-07-07 21:05:11 -0700

    mini controller finished

--- server/controllers/mini.controller.js
+++ server/controllers/mini.controller.js
@@ -9,8 +9,8 @@ module.exports = {
                 res.json(allMinis)
             })
             .catch((err)=>{
-                console.log("findAllMinis has failed");
-                res.json({message: "Something went wrong in findAllMinis", error: err})
+                console.log("findAllMinis has failed!");
+                res.json({message: "Something went wrong in findAllMinis!", error: err})
             })
     },
 
@@ -21,20 +21,49 @@ module.exports = {
             res.json(newMini);
         })
         .catch((err)=> {
-            console.log("Something went wrong with createNewMini");
+            console.log("Something went wrong with createNewMini!");
             res.status(400).json(err)
         })
     },
 
     findOneMini: (reg, res)=> {
         Miniature.findOne({_id: req.params.id})
+        .then((oneMini)=>{
+            console.log(oneMini);
+            res.json(oneMini);
+        })
+        .catch((err)=>{
+            console.log("findOneMini failed!");
+            res.json({message: "Something went wrong with findOneMini!", error: err})
+        })
+    },
+
+    deleteOneMini:  (req, res)=> {
+        Miniature.deleteOne({_id: req.params.id}) 
+        .then((deletedMini)=> {
+            console.log(deletedMini);
+            res.json(deletedMini);
+        })
+        .catch((err)=> {
+            console.log("deleteOneMovie has failed!");
+            res.json({message: "Something went wrong with deleteOneMini!", error: err}) 
+        })
+    },
+
+    updateMini:  (req, res)=> {
+        Miniature.findOneAndUpdate({_id: req.params.id},
+            req.body,
+            {new: true, runValidators: true}
+        )
+        .then((updatedMini)=> {
+            console.log(updatedMini);
+            res.json(updatedMini)
+        })
+        .catch((err)=> {
+            console.log("Something went wrong with updateMini!");
+            res.status(400).json(err)
+        })
     }
-
-
-
-
-
-
 
 
 }
