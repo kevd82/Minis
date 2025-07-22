213ebf88f28469f407c19ea8a8b7db003403eb29
Your Name <barbaricsplendor@gmail.com>
2025-07-21 22:15:29 -0700

    started AllMini.js

--- client/src/components/AllMinis.js
+++ client/src/components/AllMinis.js
@@ -1,10 +1,21 @@
-import React, {useEfffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import axios from "axios";
 
 const AllMinis = (props)=>{
 
+    const [miniList, setMiniList] = useState([]);
 
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/minis")
+        .then((res)=>{
+            console.log(res);
+            console.log(res.data);
+            setMiniList(res.data);
+        })
+        .catch((err)=>console.log(err))
+
+    }, [])
 
 
 
@@ -18,7 +29,17 @@ const AllMinis = (props)=>{
     return(
         <div>
 
-            All Minis
+            mini list stuff
+
+            {
+                miniList.map((mini, index)=>(
+                    <div key={mini._id}>
+                        <p>{mini.name}</p>
+                        <p>{mini.company}</p>
+                    </div>
+                ))
+            }
+
         </div>
     )
 
